089c49c7dc6b957393e39f848b981570b1977058
jgcohen <75163282+jgcohen@users.noreply.github.com>
2021-11-15 09:53:08 +0100

    First Commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\CallApiService;
+
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(CallApiService $callApiService): Response
+    {
+        $item = $callApiService->getData();
+
+        return $this->render(
+            'home/index.html.twig',[
+                'data' => $callApiService->getData(),
+            ]
+        );
+    }
+}
